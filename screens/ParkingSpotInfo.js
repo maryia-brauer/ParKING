83e7914f0ac0e7fa3192dc338d41eb851ace6ae3
EdijsNiks <edijsniks@gmail.com>
2023-10-16 19:54:27 +0300

    Some fixes plus reserve modal(pop up) made

--- screens/ParkingSpotInfo.js
+++ screens/ParkingSpotInfo.js
@@ -9,11 +9,13 @@ import {
   TouchableOpacity,
   Pressable,
   Radio,
+  Button,
 } from "react-native";
 import React, { useState, useMemo } from "react";
 import { useNavigation } from "@react-navigation/native";
 import Reviews from "../component/Reviews";
-//import axios from 'axios';
+import Modal from "react-native-modal";
+import Slider from "@react-native-community/slider";
 
 const ParkingSpotInfo = ({ route }) => {
   const navigation = useNavigation();
@@ -21,6 +23,15 @@ const ParkingSpotInfo = ({ route }) => {
   const [price, setPrice] = useState("free");
   const [pricePerHour, setPricePerHour] = useState("");
   const [selectedId, setSelectedId] = useState();
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [isModalReviewVisible, setModalReviewVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  const toggleModalReview = () => {
+    setModalReviewVisible(!isModalReviewVisible);
+  };
 
   const radioButtons = useMemo(
     () => [
@@ -65,7 +76,7 @@ const ParkingSpotInfo = ({ route }) => {
           flex: 1,
         }}
       >
-        <View style={{ paddingHorizontal: 10 }}>
+        <View style={{ paddingHorizontal: 10, flexDirection:'row' }}>
           <View>
             <Text
               style={{
@@ -78,7 +89,7 @@ const ParkingSpotInfo = ({ route }) => {
               Ielas nosaukums
             </Text>
           </View>
-          <View style={{ paddingHorizontal: 50, paddingVertical: 10 }}>
+          <View style={{ paddingHorizontal: 50, paddingVertical: 10, }}>
             <Image source={require("../assets/report.png")} />
           </View>
         </View>
@@ -108,11 +119,11 @@ const ParkingSpotInfo = ({ route }) => {
                 Handicap spots:
               </Text>
             </View>
-            <TouchableOpacity>       
-            <View style={{ paddingHorizontal: 140, paddingVertical: 40}}>
-              <Image source={require("../assets/fav.png")} />
-            </View>
-            </TouchableOpacity> 
+            <TouchableOpacity>
+              <View style={{ paddingHorizontal: 140, paddingVertical: 40 }}>
+                <Image source={require("../assets/fav.png")} />
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
         <View
@@ -123,10 +134,10 @@ const ParkingSpotInfo = ({ route }) => {
             left: 10,
             bottom: 10,
             right: 10,
-            paddingTop: 10 
+            paddingTop: 10,
           }}
         >
-          <Reviews/>
+          <Reviews />
         </View>
         <View
           style={{
@@ -141,15 +152,88 @@ const ParkingSpotInfo = ({ route }) => {
           >
             <Image source={require("../assets/here.png")}></Image>
           </Pressable>
-          <Pressable
+
+          {/*Reserve button*/}
+
+          <TouchableOpacity
             style={{ paddingHorizontal: 10, paddingVertical: 40 }}
-            onPress={() => navigation.navigate("Available")}
           >
-            <Image style={{}} source={require("../assets/reserve.png")}></Image>
-          </Pressable>
+            <Pressable onPress={toggleModal}>
+              <Image
+                style={{}}
+                source={require("../assets/reserve.png")}
+              ></Image>
+              <Modal
+                isVisible={isModalVisible}
+                onBackdropPress={() => setModalVisible(false)}
+                style={{ width: 300, alignSelf: "center" }}
+              >
+                <View style={{ backgroundColor: "white" }}>
+                  <View style={{ alignSelf: "center", paddingVertical: 10 }}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                      Reserve:
+                    </Text>
+                  </View>
+                  <View style={{ alignSelf: "center" }}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                      Parking space address[placeholder]
+                    </Text>
+                  </View>
+                  <View style={{ alignSelf: "center" }}>
+                    <Text style={{ fontSize: 11, paddingVertical: 7 }}>
+                      How long are you going to stay there?
+                    </Text>
+                  </View>
+                  <Slider
+                    style={{ width: 200, height: 30, alignSelf: "center" }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    minimumTrackTintColor="#E7DFDA"
+                    maximumTrackTintColor="#000000"
+                    thumbTintColor="orange"
+                  />
+                  <View style={{ alignSelf: "center" }}>
+                    <Text style={{ fontSize: 11, paddingVertical: 18 }}>
+                      2h | $6.80 [placeholders]
+                    </Text>
+                  </View>
+                  <View style={{ alignSelf: "center", paddingVertical: 5 }}>
+                    <Pressable
+                      style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingVertical: 10,
+                        paddingHorizontal: 54,
+                        borderRadius: 12,
+                        elevation: 2,
+                        backgroundColor: "#FE8F4E",
+                        bottom: 10,
+                        shadowColor: "black",
+                      }}
+                      onPress={{}}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 13,
+                          lineHeight: 16,
+                          fontWeight: "bold",
+                          letterSpacing: 0.25,
+                          color: "#0F0C0A",
+                        }}
+                      >
+                        PAY
+                      </Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </Modal>
+            </Pressable>
+          </TouchableOpacity>
         </View>
       </View>
 
+         {/*Review button */}
+      
       <View
         style={{
           flexDirection: "row",
@@ -157,11 +241,84 @@ const ParkingSpotInfo = ({ route }) => {
         }}
       >
         <Pressable
-          onPress={() => navigation.navigate("Available")}
-          style={{ paddingVertical: 50, paddingHorizontal: 35 }}
+          onPress={toggleModalReview}
+          style={{ paddingVertical: 50, paddingHorizontal: 35,  }}
         >
           <Image source={require("../assets/review.png")}></Image>
+          <Modal
+            isVisible={isModalReviewVisible}
+            onBackdropPress={() => setModalReviewVisible(false)}
+            style={{   backgroundColor: "white", alignItems: "center" }}
+          >
+            <View style={{ backgroundColor: "white" }}>
+              <View style={{  paddingVertical: 10 }}>
+                <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                  Write a review
+                </Text>
+              </View>
+            </View>
+
+            <View
+              style={{
+                marginTop: 20,
+                
+                backgroundColor: "grey",
+                paddingVertical: 8,
+                borderRadius: 8,
+                border: "solid black",
+                borderWidth: 0.5,
+                gap: 5,
+              }}
+            >
+              <View style={{ width:300, paddingVertical: 10 }}>
+                <TextInput
+                  placeholder="Title"
+                  style={{ fontWeight: "bold", fontSize: 15 }}
+                />
+              </View>
+            </View>
+            <View style={{ backgroundColor: "grey" }}>
+              <View style={{ width:300, paddingVertical:20 }}>
+                <TextInput
+                  placeholder="Review message..."
+                  style={{ fontWeight: "bold", fontSize: 15 }}
+                />
+              </View>
+
+              <View style={{ alignSelf: "center", paddingVertical: 5, bottom:-100 }}>
+                <Pressable
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    paddingVertical: 10,
+                    paddingHorizontal: 54,
+                    borderRadius: 12,
+                    elevation: 2,
+                    backgroundColor: "#FE8F4E",
+                    
+                    shadowColor: "black",
+                  }}
+                  onPress={{}}
+                >
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      lineHeight: 16,
+                      fontWeight: "bold",
+                      letterSpacing: 0.25,
+                      color: "#0F0C0A",
+                    }}
+                  >
+                    Submit
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
         </Pressable>
+
+              {/*Review */}
+
         <Pressable
           style={{ paddingHorizontal: 35, paddingVertical: 50 }}
           onPress={() => navigation.navigate("Available")}
